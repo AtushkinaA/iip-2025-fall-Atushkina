95e4645bf577630091dca21a43fcd227005cecbf
AtushkinaA <119050696+AtushkinaA@users.noreply.github.com>
2025-09-19 17:10:36 +0300

    Delete z0773.java

--- z0773.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class z0773 {
-    public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	int k = sc.nextInt();
-	int m = sc.nextInt();
-	System.out.println(k*k*m);
-    }
-}
