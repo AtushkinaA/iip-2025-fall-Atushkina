84adf2b35ff68bd6588feebaa488e361a80ed02c
AtushkinaA <119050696+AtushkinaA@users.noreply.github.com>
2025-09-19 17:08:45 +0300

    Delete z0108.java

--- z0108.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class z0108 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int firstch = sc.nextInt();
-	System.out.println(firstch);
-    }
-}
